76faace7ea14587560a1641846e29d76a2069f19
akira <leejmcoco@gmail.com>
2019-05-21 23:04:08 +0900

    Bookmark Service

---
+++ bookmark/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from .views import BookmarkListView
+from .views import BookmarkCreateView
+from .views import BookmarkDetailView
+from .views import BookmarkUpdateView
+from .views import BookmarkDeleteView
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('', BookmarkListView.as_view(), name='list'),
+    path('add/', BookmarkCreateView.as_view(), name='add'),
+    path('detail/<int:pk>', BookmarkDetailView.as_view(), name="detail"),
+    path('update/<int:pk>', BookmarkUpdateView.as_view(), name="update"),
+    path('delete/<int:pk>', BookmarkDeleteView.as_view(), name="delete"),
+]
